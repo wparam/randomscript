c4e9707b9d6b27116062910c3a1769ff047c0ce4
wparam@hotmail.com <wparam@hotmail.com>
2016-08-14 22:17:44 +0800

    up

--- juery-plugins/star.js
+++ juery-plugins/star.js
@@ -1,8 +1,11 @@
 (function($){
     'use strict';
-
+    console.log(this);
     $.fn.starIt = function(){
         console.log('hit star it');
+        console.log(this);
+        console.log(arguments);
+        $.widget('',{});
     };
 
-})(jQuery);
+}(jQuery));
